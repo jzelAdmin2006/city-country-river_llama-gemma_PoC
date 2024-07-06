43f0ebc53af32f4210648f29791a961b460d9906
Johannes Zeller <jo.zeller@protonmail.com>
2024-07-06 14:38:59 +0200

    initial commit

---
+++ src/main/java/com/jzel/citycountryriver_llama/CityCountryRiverLlamaApplication.java
@@ -0,0 +1,13 @@
+package com.jzel.citycountryriver_llama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCountryRiverLlamaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CityCountryRiverLlamaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jzel/citycountryriver_llama/CityCountryRiverLlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzel.citycountryriver_llama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCountryRiverLlamaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
